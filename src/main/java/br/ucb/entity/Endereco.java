d300ce4c79a582d67aca2ca4a9a9ad5005ebf3d4
andre.sousa <andreg.sousa@hotmail.com>
2017-12-18 22:23:31 -0200

    mapeando endereço

---
+++ src/main/java/br/ucb/entity/Endereco.java
@@ -0,0 +1,90 @@
+package br.ucb.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idEndereco;
+	
+	@Column(name = "estado")
+	private String estado;
+	
+	@Column(name = "bairro")
+	private String bairro;
+	
+	@Column(name = "cidade")
+	private String cidade;
+	
+	@Column(name = "complemento")
+	private String complemento;
+	
+	@Column(name = "rua")
+	private String rua;
+	
+	@Column(name = "numero")
+	private Integer numero;
+
+	public Integer getIdEndereco() {
+		return this.idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public String getEstado() {
+		return this.estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getBairro() {
+		return this.bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return this.cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getComplemento() {
+		return this.complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getRua() {
+		return this.rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public Integer getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+}
