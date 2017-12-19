d8fd03a8588f24deaa91a3e73e148ea5c35a341f
andre.sousa <andreg.sousa@hotmail.com>
2017-12-18 23:00:01 -0200

    mapeando tipo producao

---
+++ src/main/java/br/ucb/entity/TipoProducao.java
@@ -0,0 +1,46 @@
+package br.ucb.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TipoProducao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idTipoProducao;
+
+	@Column(name = "tipo")
+	private String tipo;
+
+	@Column(name = "descricao")
+	private String descricao;
+
+	public Integer getIdTipoProducao() {
+		return this.idTipoProducao;
+	}
+
+	public void setIdTipoProducao(Integer idTipoProducao) {
+		this.idTipoProducao = idTipoProducao;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
