bf83a11a0231149fb9cb07c7b75c75261f2e6af9
andre.sousa <andreg.sousa@hotmail.com>
2018-01-11 23:04:48 -0200

    Mapeando a entidade curso

---
+++ src/main/java/br/ucb/entity/Curso.java
@@ -0,0 +1,35 @@
+package br.ucb.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Curso {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCurso;
+	
+	@Column(name = "nome")
+	private String nome;
+
+	public Integer getIdCurso() {
+		return this.idCurso;
+	}
+
+	public void setIdCurso(Integer idCurso) {
+		this.idCurso = idCurso;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
