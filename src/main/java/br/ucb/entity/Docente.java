0b8437a0c6e0292325fb0e833faad7dfa521cf6d
andre.sousa <andreg.sousa@hotmail.com>
2018-01-18 23:12:24 -0200

    Mapeando docente

--- src/main/java/br/ucb/entity/Docente.java
+++ src/main/java/br/ucb/entity/Docente.java
@@ -1,5 +1,185 @@
 package br.ucb.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Docente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idDocente;
+	
+	@Column(name = "matricula")
+	private String matricula;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "situacao")
+	private String situacao;
+	
+	@Column(name = "sexo")
+	private char sexo;
+	
+	@Column(name  = "celular")
+	private String celular;
+	
+	@Column(name = "telefoneFixo")
+	private String telefoneFixo;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "ativo")
+	private boolean ativo;
+	
+	@Column(name = "dataNascimento")
+	private Date dataNascimento;
+	
+	@Column(name = "raml")
+	private String ramal;
+	
+	@Column(name = "dataCadastro")
+	private Date dataCadastro;
+	
+	@ManyToOne
+	@JoinColumn
+	private TipoDocente tipoDocente;
+	
+	@ManyToOne
+	@JoinColumn(name = "idEndereco")
+	private Endereco endereco;
+	
+	@ManyToOne
+	@JoinColumn(name = "idCurso")
+	private Curso curso;
+
+	public Integer getIdDocente() {
+		return this.idDocente;
+	}
+
+	public void setIdDocente(Integer idDocente) {
+		this.idDocente = idDocente;
+	}
+
+	public String getMatricula() {
+		return this.matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSituacao() {
+		return this.situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public char getSexo() {
+		return this.sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCelular() {
+		return this.celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getTelefoneFixo() {
+		return this.telefoneFixo;
+	}
+
+	public void setTelefoneFixo(String telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAtivo() {
+		return this.ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Date getDataNascimento() {
+		return this.dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRamal() {
+		return this.ramal;
+	}
+
+	public void setRamal(String ramal) {
+		this.ramal = ramal;
+	}
+
+	public Date getDataCadastro() {
+		return this.dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public TipoDocente getTipoDocente() {
+		return this.tipoDocente;
+	}
+
+	public void setTipoDocente(TipoDocente tipoDocente) {
+		this.tipoDocente = tipoDocente;
+	}
+
+	public Endereco getEndereco() {
+		return this.endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Curso getCurso() {
+		return this.curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
 
 }
