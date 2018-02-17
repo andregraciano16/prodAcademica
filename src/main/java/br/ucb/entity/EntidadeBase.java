db1410bd6b0059156fc1e985974511c16ee725bc
andre.sousa <andreg.sousa@hotmail.com>
2018-02-17 18:25:34 -0200

    criando entidade base

---
+++ src/main/java/br/ucb/entity/EntidadeBase.java
@@ -0,0 +1,9 @@
+package br.ucb.entity;
+
+import java.io.Serializable;
+
+public class EntidadeBase implements Serializable{
+
+	private static final long serialVersionUID = -100011688495466489L;
+
+}
