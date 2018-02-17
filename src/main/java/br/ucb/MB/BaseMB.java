34e53449cd8158cc9a3dbb731fa368875e830016
andre.sousa <andreg.sousa@hotmail.com>
2018-02-17 18:22:44 -0200

    criando  base mb

---
+++ src/main/java/br/ucb/MB/BaseMB.java
@@ -0,0 +1,12 @@
+package br.ucb.MB;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+
+public abstract class BaseMB implements Serializable{
+
+	private static final long serialVersionUID = 6679596452844233836L;
+
+
+}
