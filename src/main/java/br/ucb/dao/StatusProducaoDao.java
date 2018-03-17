33553c8c0a262bdfd1723605ae8862dc6b3ef584
andre.sousa <andreg.sousa@hotmail.com>
2018-03-17 09:05:39 -0300

    correção

--- src/main/java/br/ucb/dao/StatusProducaoDao.java
+++ src/main/java/br/ucb/dao/StatusProducaoDao.java
@@ -6,10 +6,6 @@ import br.ucb.entity.StatusProducao;
 
 public interface StatusProducaoDao extends DaoGenerico<StatusProducao, Integer> {
 
-<<<<<<< HEAD
-	List<StatusProducao>  findByDescricaoAndTipo(String tipo, String descricao);
-=======
 	List<StatusProducao> findByDescricaoAndTipo(String tipo, String descricao);
->>>>>>> branch 'master' of https://github.com/andregraciano16/prodAcademica.git
 
 }
