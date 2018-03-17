f24f2133444e912768a1ea36f05ef6a9b2510c63
Daniel Queiroz <queirozdefino@gmail.com>
2018-03-17 13:05:47 -0300

    Commit StatusProducaoDao

--- src/main/java/br/ucb/dao/StatusProducaoDao.java
+++ src/main/java/br/ucb/dao/StatusProducaoDao.java
@@ -5,11 +5,7 @@ import java.util.List;
 import br.ucb.entity.StatusProducao;
 
 public interface StatusProducaoDao extends DaoGenerico<StatusProducao, Integer> {
-
-<<<<<<< HEAD
-	List<StatusProducao>  findByDescricaoAndTipo(String tipo, String descricao);
-=======
+	
 	List<StatusProducao> findByDescricaoAndTipo(String tipo, String descricao);
->>>>>>> branch 'master' of https://github.com/andregraciano16/prodAcademica.git
 
 }
