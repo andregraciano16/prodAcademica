38b6a04482ccd42310f28c8ec0d4b4a8ba4591b8
Daniel Queiroz <queirozdefino@gmail.com>
2018-03-18 13:15:35 -0300

    Classes Conflit

--- src/main/java/br/ucb/MB/BaseMB.java
+++ src/main/java/br/ucb/MB/BaseMB.java
@@ -2,10 +2,22 @@ package br.ucb.MB;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 
 public abstract class BaseMB implements Serializable{
 
 	private static final long serialVersionUID = 6679596452844233836L;
 
+	public void setMessageSuccess(String mensagem){
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, 
+        		mensagem, mensagem));
+	}
+	
+	public void setMessageError(String mensagem){
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+        		mensagem, mensagem));
+	}
 
 }
