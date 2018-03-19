e5375d13646963b4f3e4530271b0873323dfc80c
andre.sousa <andreg.sousa@hotmail.com>
2018-03-19 01:46:47 -0300

    Ajustes

--- src/main/java/br/ucb/MB/DocenteMB.java
+++ src/main/java/br/ucb/MB/DocenteMB.java
@@ -8,11 +8,17 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.ucb.dao.CursoDao;
 import br.ucb.dao.DocenteDao;
+import br.ucb.dao.EnderecoDao;
 import br.ucb.dao.TipoDocenteDao;
+import br.ucb.dao.impl.CursoDaoImpl;
 import br.ucb.dao.impl.DocenteDaoImpl;
+import br.ucb.dao.impl.EnderecoDaoImpl;
 import br.ucb.dao.impl.TipoDocenteDaoImpl;
+import br.ucb.entity.Curso;
 import br.ucb.entity.Docente;
+import br.ucb.entity.Endereco;
 import br.ucb.entity.TipoDocente;
 
 @ManagedBean(name = "docenteMB")
@@ -23,9 +29,13 @@ public class DocenteMB extends BaseMB{
 	
 	private DocenteDao docenteDao;
 	private TipoDocenteDao tipoDocenteDao;
+	private CursoDao cursoDao;
 	private Docente docente;
+	private Endereco endereco;
 	private List<Docente> docentes;
 	private TipoDocente tipoDocente;
+	private Curso curso;
+	private EnderecoDao enderecoDao;
 	
 	@PostConstruct
 	public void init() {
@@ -34,10 +44,21 @@ public class DocenteMB extends BaseMB{
 		this.tipoDocenteDao = new TipoDocenteDaoImpl();
 		this.docente        = new Docente();
 		this.tipoDocente    = new TipoDocente(); 
+		this.cursoDao       = new CursoDaoImpl();
+		this.curso          = new Curso();
+		this.endereco       = new Endereco();
+		this.enderecoDao    = new EnderecoDaoImpl();
 	}
 
 	public void cadastrar() {
 		this.docente.setDataCadastro(new Date());
+		this.tipoDocente = this.tipoDocenteDao.findByKey(TipoDocente.class, tipoDocente.getIdTipoDocente());
+		this.curso       = this.cursoDao.findById(curso.getIdCurso());
+		this.enderecoDao.save(this.endereco);
+		this.endereco = this.enderecoDao.find(this.endereco);
+		this.docente.setEndereco(endereco);
+		this.docente.setCurso(this.curso);
+		this.docente.setDataNascimento(new Date());
 		this.docente.setTipoDocente(this.tipoDocente);
 		this.docenteDao.save(this.docente);
 	}
@@ -60,6 +81,10 @@ public class DocenteMB extends BaseMB{
 	
 	public List<TipoDocente> getTipoDocentes(){
 		return tipoDocenteDao.list();
+	}
+	
+	public List<Curso> getCursos(){
+		return cursoDao.list();
 	}
 
 	public Docente getDocente() {
@@ -85,7 +110,23 @@ public class DocenteMB extends BaseMB{
 	public void setTipoDocente(TipoDocente tipoDocente) {
 		this.tipoDocente = tipoDocente;
 	}
-	
-	
 
+	public Endereco getEndereco() {
+		return this.endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Curso getCurso() {
+		return this.curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
+	
+	
 }
