618ad30d56eb7e2658dd945c81df3b2c9b1acb6c
Daniel Queiroz <queirozdefino@gmail.com>
2018-03-19 23:42:24 -0300

    Correção de telas. Algumas apresetão erros na busca

---
+++ src/main/java/br/ucb/converter/AlunoConverter.java
@@ -0,0 +1,37 @@
+package br.ucb.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.ucb.dao.AlunoDao;
+import br.ucb.dao.impl.AlunoDaoImpl;
+import br.ucb.entity.Aluno;
+import br.ucb.entity.Aluno;
+
+
+
+@FacesConverter(forClass = Aluno.class)
+public class AlunoConverter implements Converter{
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		Aluno retorno = null;
+
+		if (value != null) {
+			AlunoDao docenteDao = new AlunoDaoImpl();
+			retorno = docenteDao.findById(new Integer(value));
+		}
+
+		return retorno;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value != null) {
+			return ((Aluno) value).getIdAluno().toString();
+		}
+		return null;
+	}
+}
