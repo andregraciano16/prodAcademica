855c89a133bedb12f6ac5e1442d4b1eb589e2863
Daniel Queiroz <queirozdefino@gmail.com>
2018-04-23 11:42:51 -0300

    Conflitos: ProduçãoAcademica e Template

--- src/main/java/br/ucb/entity/ProducaoAcademica.java
+++ src/main/java/br/ucb/entity/ProducaoAcademica.java
@@ -9,11 +9,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-public class ProducaoAcademica extends EntidadeBase {
+public class ProducaoAcademica extends EntidadeBase implements Comparable<ProducaoAcademica>{
 
 	private static final long serialVersionUID = 5005969022033064082L;
 
@@ -27,21 +28,6 @@ public class ProducaoAcademica extends EntidadeBase {
 
 	@Column(name = "descricao")
 	private String descricao;
-
-	@Column(name = "nomeRevista")
-	private String nomeRevista;
-
-	@Column(name = "paginaInicial")
-	private Integer paginaIncial;
-
-	@Column(name = "paginaFinal")
-	private Integer paginaFinal;
-
-	@Column(name = "ano")
-	private Integer ano;
-
-	@Column(name = "ISSN")
-	private Integer ISSN;
 
 	@Column(name = "conceitoQualis")
 	private String conceitoQualis;
@@ -87,46 +73,6 @@ public class ProducaoAcademica extends EntidadeBase {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
-	}
-
-	public String getNomeRevista() {
-		return this.nomeRevista;
-	}
-
-	public void setNomeRevista(String nomeRevista) {
-		this.nomeRevista = nomeRevista;
-	}
-
-	public Integer getPaginaIncial() {
-		return this.paginaIncial;
-	}
-
-	public void setPaginaIncial(Integer paginaIncial) {
-		this.paginaIncial = paginaIncial;
-	}
-
-	public Integer getPaginaFinal() {
-		return this.paginaFinal;
-	}
-
-	public void setPaginaFinal(Integer paginaFinal) {
-		this.paginaFinal = paginaFinal;
-	}
-
-	public Integer getAno() {
-		return this.ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public Integer getISSN() {
-		return this.ISSN;
-	}
-
-	public void setISSN(Integer iSSN) {
-		this.ISSN = iSSN;
 	}
 
 	public String getConceitoQualis() {
@@ -211,6 +157,11 @@ public class ProducaoAcademica extends EntidadeBase {
 
 	public int hashCode() {
 		return this.getDescricao().hashCode();
+	}
+
+	@Override
+	public int compareTo(ProducaoAcademica outro) {
+		return getDataCadastro().compareTo(outro.getDataCadastro());
 	}
 
 }
