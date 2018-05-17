d36608ebdd8ed4e03789833e1436b5b0102efb62
Daniel Queiroz <queirozdefino@gmail.com>
2018-05-17 14:02:06 -0300

    Correção de cadastro de DOCENTE e adição de Histórico na mesma.

--- src/main/java/br/ucb/MB/DocenteMB.java
+++ src/main/java/br/ucb/MB/DocenteMB.java
@@ -12,16 +12,21 @@ import br.ucb.VO.DocenteVO;
 import br.ucb.dao.CursoDao;
 import br.ucb.dao.DocenteDao;
 import br.ucb.dao.EnderecoDao;
+import br.ucb.dao.HistoricoDao;
 import br.ucb.dao.TipoDocenteDao;
 import br.ucb.dao.impl.CursoDaoImpl;
 import br.ucb.dao.impl.DocenteDaoImpl;
 import br.ucb.dao.impl.EnderecoDaoImpl;
+import br.ucb.dao.impl.HistoricoDaoImpl;
 import br.ucb.dao.impl.TipoDocenteDaoImpl;
 import br.ucb.entity.Curso;
 import br.ucb.entity.Docente;
 import br.ucb.entity.Endereco;
+import br.ucb.entity.Historico;
 import br.ucb.entity.TipoDocente;
 import br.ucb.enums.AcaoEnum;
+import br.ucb.security.Seguranca;
+import br.ucb.security.UsuarioSistema;
 
 @ManagedBean(name = "docenteMB")
 @ViewScoped
@@ -39,6 +44,9 @@ public class DocenteMB extends BaseMB{
 	private EnderecoDao enderecoDao;
     private AcaoEnum acaoEnum;
     private DocenteVO docentePesq;
+	private Historico historico;
+	private HistoricoDao historicoDao;
+	private UsuarioSistema user;
 
 	
 	@PostConstruct
@@ -57,6 +65,9 @@ public class DocenteMB extends BaseMB{
 		this.curso          = new Curso();
 		this.docentePesq    = new DocenteVO();
 		this.docente.setEndereco(new Endereco());
+		this.historico		= new Historico();
+		this.historicoDao 	= new HistoricoDaoImpl();
+		this.user 			= new Seguranca().getUsuarioLogado();
 		this.acaoEnum       = AcaoEnum.LISTAR;
 	}
 	
@@ -76,6 +87,7 @@ public class DocenteMB extends BaseMB{
 	public void editar(){
 		this.enderecoDao.save(this.docente.getEndereco());
 		this.docenteDao.update(this.docente);
+		cadastraHistorico("Foi alterado com sucesso.", this.docente);
 		setMessageSuccess("Atualizado com sucesso!");
 	}
 
@@ -87,9 +99,20 @@ public class DocenteMB extends BaseMB{
 		this.docente.setCurso(this.curso);
 		this.docente.setDataNascimento(new Date());
 		this.docente.setTipoDocente(this.tipoDocente);
+		this.docente.setEndereco(this.enderecoDao.find(this.docente.getEndereco()));
 		this.docenteDao.save(this.docente);
+		cadastraHistorico("Foi cadastrado com sucesso.", this.docenteDao.getDocentebyMatricula(this.docente.getMatricula()));
 		setMessageSuccess("Cadastrado com sucesso!");
 	}
+	
+	public void cadastraHistorico(String mensagem, Docente docente) {
+		this.historico.setDataAlteracao(new Date());
+		this.historico.setDocente(docente);
+		this.historico.setAlteracao("Docente: " + docente.getNome() + "\n" + "Matrícula: " + docente.getMatricula() + "\n"
+				+ mensagem + "\n" + "Responsável: " + this.docenteDao.getDocentebyMatricula(user.getUsuario().getMatricula()).getNome());
+		this.historicoDao.save(historico);
+	}
+
 	
 	public AcaoEnum getAcaoEnum() {
 		return this.acaoEnum;
@@ -117,6 +140,7 @@ public class DocenteMB extends BaseMB{
         if(tipo.isAtivo()){
         	tipo.setAtivo(Boolean.FALSE);
 			this.docenteDao.update(tipo);
+			cadastraHistorico("Foi desativado.", tipo);
 			setMessageSuccess("Inativado com sucesso!");
         }else{
         	setMessageError("Docente já está inativado!");
@@ -134,6 +158,8 @@ public class DocenteMB extends BaseMB{
 	public void limpar() {
 		inicializar();
 	}
+	
+	
 	
 	public List<TipoDocente> getTipoDocentes(){
 		return tipoDocenteDao.list();
